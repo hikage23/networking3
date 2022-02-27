fab052325df7da43f855b2e7580ed5010dca6aa5
Numaan Cheema <hikage23@gmail.com>
2022-02-26 22:53:47 -0500

    Attempt #8

--- solution.py
+++ solution.py
@@ -5,74 +5,65 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     msg = "\r\n This is My message"
     endmsg = "\r\n.\r\n"
 
-    # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
 
     # Fill in start
+    mailServer = (mailserver,port)
     clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect(mailserver,port)
+    clientSocket.connect(mailServer)
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
-    #print(recv) #You can use these print statement to validate return codes from the server.
     if recv[:3] != '220':
        print('220 reply not received from server.')
 
     # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
-    recv1 = clientSocket.recv(1024).decode()
-    #print(recv1)
+    recv1 = clientSocket.recv(1024)
+    print(recv1)
     if recv1[:3] != '250':
        print('250 reply not received from server.')
 
     # Send MAIL FROM command and handle server response.
-    # Fill in start
     mailFromCommand = "MAIL FROM: <mailfrom@email.com> \r\n"
     clientSocket.send(mailFromCommand.encode())
-    recv2 = clientSocket.recv(1024).decode()
+    recv2 = clientSocket.recv(1024)
+    print(recv2)
     if recv2[:3] != '250':
        print('250 reply not received from server.')
-    # Fill in end
 
     # Send RCPT TO command and handle server response.
-    # Fill in start
     rcptToCommand = "RCPT TO: <rcptto@email.com> \r\n"
     clientSocket.send(rcptToCommand.encode())
-    recv3 = clientSocket.recv(1024).decode()
+    recv3 = clientSocket.recv(1024)
+    print(recv3)
     if recv3[:3] != '250':
        print('250 reply not received from server.')
-    # Fill in end
 
     # Send DATA command and handle server response.
     # Fill in start
     dataCommand = "DATA \r\n"
     clientSocket.send(dataCommand.encode())
-    recv4 = clientSocket.recv(1024).decode()
-    if recv4[:3] != '354':
-       print('354 reply not received from server.')
+    recv4 = clientSocket.recv(1024)
+    if recv4[:3] != '250':
+       print('250 reply not received from server.')
     # Fill in end
 
     # Send message data.
-    # Fill in start
-    clientSocket.send(msg.encode())
-    # Fill in end
+    clientSocket.send("\r\n This is My message".encode())
 
     # Message ends with a single period, send message end and handle server response.
-    # Fill in start
-    clientSocket.send(endmsg.encode())
+    clientSocket.send("\r\n.\r\n".encode())
     recv5 = clientSocket.recv(1024)
     if recv5[:3] != '250':
        print('250 reply not received from server.')
-    # Fill in end
 
     # Send QUIT command and handle server response.
     # Fill in start
     quitCommand = "QUIT \r\n"
-    clientSocket.send(quitCommand.encode())
+    clientSocket.send(quitCommand)
     clientSocket.close()
-    pass
     # Fill in end
 
 

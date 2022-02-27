34f6255595660c095c520403832fa3cd09ba2d40
Numaan Cheema <hikage23@gmail.com>
2022-02-26 22:10:32 -0500

    Attempt #6

--- solution.py
+++ solution.py
@@ -71,8 +71,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Fill in start
     quitCommand = "QUIT \r\n"
     clientSocket.send(quitCommand.encode())
-    recv7 = clientSocket.recv(1024)
     clientSocket.close()
+    pass
     # Fill in end
 
 
